7488491d33a0afcdfe191f4b8b03189e3303ed53
zz <549024307@qq.com>
2020-07-04 10:38:14 +0800

    init

---
+++ precache-manifest.d5114d022211ba4d2ec240621209a84e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782a40b0107aa5cc2ce3",
+    "url": "/css/app.77161df4.css"
+  },
+  {
+    "revision": "670cd535e3e6bc5e939f",
+    "url": "/css/chunk-36062e74.0e433876.css"
+  },
+  {
+    "revision": "fedb00117629bbb8cf7df9581c1d07aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "782a40b0107aa5cc2ce3",
+    "url": "/js/app.e07abfb2.js"
+  },
+  {
+    "revision": "670cd535e3e6bc5e939f",
+    "url": "/js/chunk-36062e74.17e2f6b0.js"
+  },
+  {
+    "revision": "1f4376474effee32035d",
+    "url": "/js/chunk-vendors.420e401e.js"
+  },
+  {
+    "revision": "4a1fe432f6bb12d32f6ab2ee625c24e8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
